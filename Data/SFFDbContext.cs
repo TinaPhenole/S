ae16b62e08ede12fd6b2863e010bd81534916cfd
TinaPhenole <91chrjoh@skola.boras.se>
2022-05-17 10:25:54 +0200

    Added controlles and fixed genre to a string in movie

--- Data/SFFDbContext.cs
+++ Data/SFFDbContext.cs
@@ -1,6 +1,7 @@
-﻿using System;
+using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
+using SFF_Komplettering;
 
 #nullable disable
 
@@ -21,7 +22,6 @@ namespace SFF_Komplettering.Data
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
                 optionsBuilder.UseSqlite("Data Source=SFFDb.db;");
             }
         }
@@ -32,5 +32,13 @@ namespace SFF_Komplettering.Data
         }
 
         partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+
+        public DbSet<SFF_Komplettering.Movie> Movie { get; set; }
+
+        public DbSet<SFF_Komplettering.FilmAssociation> FilmAssociation { get; set; }
+
+        public DbSet<SFF_Komplettering.Rental> Rental { get; set; }
+
+        public DbSet<SFF_Komplettering.Review> Review { get; set; }
     }
 }
--- Models/Movie.cs
+++ Models/Movie.cs
@@ -1,15 +1,13 @@
 using System;
 using System.ComponentModel.DataAnnotations;
 using Newtonsoft.Json;
-using Newtonsoft.Json.Converters;
 namespace SFF_Komplettering
 {
     public class Movie : BaseEntity
     {
         [Required]
         public string Title { get; set; }
-        [JsonConverter(typeof(StringEnumConverter))]
-        public Genre Genre { get; set; }
+        public string Genre { get; set; }
         public DateTime ReleaseDate { get; set; }
         [Required]
         public string Description { get; set; }
